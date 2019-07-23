ac12fa00d737faa793c00b4fc3712ad18754dfe7
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-23 11:47:15 -0700

    GET gets all the reviews and passes test

--- test/reviews.test.js
+++ test/reviews.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
-// const Review = require('../lib/models/Review');
+const Review = require('../lib/models/Review');
 
 describe('app routes', () => {
     beforeAll(() => {
@@ -40,7 +40,41 @@ describe('app routes', () => {
             });
     });
 
-    
+    it('gets all reviews with GET', async() => {
+        const reviews = await Review.create([{
+            reviewer: 'Me',
+            distance: 123,
+            difficulty: 9,
+            review: 'Some boring trail.'
+        }, {
+            reviewer: 'Someone Else',
+            distance: 28,
+            difficulty: 2,
+            review: 'Some other boring trail.'
+        }, {
+            reviewer: 'Joe',
+            distance: 3,
+            difficulty: 1,
+            review: 'Some exciting trail.'
+        },
+        ]);
+
+        return request(app)
+            .get('/api/v1/reviews')
+            .then(res => {
+                const reviewsJSON = JSON.parse(JSON.stringify(reviews));
+                reviewsJSON.forEach(review => {
+                    expect(res.body).toContainEqual({
+                        _id: review._id,
+                        reviewer: review.reviewer,
+                        distance: review.distance,
+                        difficulty: review.difficulty,
+                        review: review.review,
+                        __v: 0
+                    });
+                });
+            });
+    });
 
     // reviewer: 
     // distance: 
